d7230f534a30d13415bfa9d5a94e19d73b1221b8
Nico <pascualnicolass@gmail.com>
2018-10-20 20:34:03 -0300

    last commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiURl: 'http://node-backend-node-backend.7e14.starter-us-west-2.openshiftapps.com/api/'
+  apiURl: 'https://node-backend-node-backend.7e14.starter-us-west-2.openshiftapps.com/api/'
 };
